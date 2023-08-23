14da24931b69796259eae72c3e48d85cb5d81e3e
akshaychavhan <akshay@rtledgers.com>
2023-08-23 12:30:52 +0530

    changes done

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
 const jsonFile = require("jsonfile");
 const moment = require("moment");
 const simpleGit = require("simple-git"); // this is to commit commands
-const { Random, MersenneTwister19937 } = require("random-js");
+
 
 
 
 const FILE_PATH = "./data.json";
 
-const makeCommit = (n) => {
-
-    if (n === 0) return simpleGit().push();
-
-    
-    const random = new Random(MersenneTwister19937.autoSeed());
-    const x = random.integer(10, 54);
-    const y = random.integer(0, 6);
+const makeCommit = (x, y) => {
     // const DATE = moment().format();    // todays date
-    // const DATE = moment().subtract(2, 'd').format();   // 1 day before todays date
     // const DATE = moment().subtract(1 ,'y').add(2 ,'d').format();   // substract 1 year and add 2 day from current
 
-    const DATE = moment().subtract(2, 'd').
-        add(x, 'w').add(y, 'd').format();   // 1 day before todays date
+    // const DATE = moment().subtract(2, 'd').format();   // 1 day before todays date
 
+    const DATE = moment().subtract(1, 'y').add(2, 'd')
+        .add(x, 'w').add(y, 'd').format();
     const data = {
         date: DATE
     }
 
     jsonFile.writeFile(FILE_PATH, data, () => {
-        simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE },
-            makeCommit.bind(this, --n));
+        simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE }).push();
     });
-
 }
 
-makeCommit(50);
-
+makeCommit(2, 3);
 
