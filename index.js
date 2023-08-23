aa75fb49e8bf8a99f422dbcfa4848c0ae01619c6
akshaychavhan <akshay@rtledgers.com>
2023-08-23 12:43:48 +0530

    File changes need to work more

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const simpleGit = require("simple-git"); // this is to commit commands
 
 const FILE_PATH = "./data.json";
 
-const makeCommit = (x, y) => {
+const makeCommit = async (n) => {
+    console.log("simple => ", simpleGit);
+    if(n === 0) return await simpleGit().push();
+
+    const x = Math.floor(Math.random() * 54); 
+    const y = Math.floor(Math.random() * 6);
     // const DATE = moment().format();    // todays date
     // const DATE = moment().subtract(1 ,'y').add(2 ,'d').format();   // substract 1 year and add 2 day from current
 
@@ -20,9 +25,10 @@ const makeCommit = (x, y) => {
     }
 
     jsonFile.writeFile(FILE_PATH, data, () => {
-        simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE }).push();
+        simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE },
+        makeCommit.bind(this , --n));
     });
 }
 
-makeCommit(2, 3);
+makeCommit(50);
 
